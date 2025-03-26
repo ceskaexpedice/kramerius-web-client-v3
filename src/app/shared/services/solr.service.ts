d4a59c29e8b566ad61235cba7316a280ae576fde
marianharbist <marianharbist@gmail.com>
2025-03-26 14:30:44 +0100

    created solr service with getPeriodicals and getBooks functions

---
+++ src/app/shared/services/solr.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolrService {
+  private readonly API_URL = 'https://api.kramerius.mzk.cz/search/api/client/v7.0/search';
+
+  constructor(private http: HttpClient) {}
+
+  getPeriodicals(): Observable<any> {
+    const params = {
+      q: '*:*',
+      fq: 'model:periodical',
+      sort: 'created desc',
+      rows: 100,
+      start: 0,
+      wt: 'json'
+    };
+
+    return this.http.get<any>(this.API_URL, { params });
+  }
+
+  getBooks(): Observable<any> {
+    const params = {
+      q: '*:*',
+      fq: 'model:monograph',
+      sort: 'created desc',
+      rows: 100,
+      start: 0,
+      wt: 'json'
+    }
+
+    return this.http.get<any>(this.API_URL, { params });
+  }
+
+  // ďalšie metódy: getBooks(), getGenres(), getAuthors() atď.
+}
