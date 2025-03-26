3c336ab8dce573d8f6a16fca42965e894ae105d2
marianharbist <marianharbist@gmail.com>
2025-03-26 14:30:15 +0100

    updated header component, fixed overflow x due to lang picker absolute position

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import {LangPickerComponent} from '../../../shared/translation/lang-picker/lang-picker.component';
 import {TranslatePipe} from '@ngx-translate/core';
+import {Router} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   imports: [
-    LangPickerComponent
+    LangPickerComponent,
+    NgIf,
   ],
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  get showSearchBar(): boolean {
+    return this.router.url !== '/search';
+  }
 
 }
