64e2790af817db04c48e8eca205229a227a086b6
marianharbist <marianharbist@gmail.com>
2025-04-23 16:12:17 +0200

    autocomplete restyled - work in progress

--- src/app/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -1,73 +1,68 @@
 import {Component, effect, inject, Input, OnInit, signal} from '@angular/core';
-import {NgClass, NgForOf, NgIf} from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
+import {NgClass, AsyncPipe} from '@angular/common';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {SolrService} from '../../../core/solr/solr.service';
 import {TranslatePipe} from '@ngx-translate/core';
 import {SearchService} from '../../services/search.service';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {Observable, of, debounceTime, switchMap, startWith} from 'rxjs';
 
 @Component({
   selector: 'app-autocomplete',
+  standalone: true,
   imports: [
-    NgIf,
-    NgForOf,
+    NgClass,
     ReactiveFormsModule,
     TranslatePipe,
-    NgClass,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    AsyncPipe
   ],
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.scss',
 })
 export class AutocompleteComponent implements OnInit {
-  inputTerm = signal('');
-  suggestions = signal<string[]>([]);
+  searchControl = new FormControl('');
   isLoading = signal(false);
+  filteredOptions!: Observable<string[]>;
 
-  private solrService = inject(SolrService)
+  private solrService = inject(SolrService);
   private searchService = inject(SearchService);
 
   @Input('inputTheme') inputTheme: string = 'light';
 
-  constructor(
-  ) {
-
-    effect(() => {
-      const term = this.inputTerm();
-      if (!term || term.length < 2) {
-        this.suggestions.set([]);
-        return;
-      }
-
-      this.isLoading.set(true);
-      const sub = this.solrService.getAutocompleteSuggestions(term).subscribe({
-        next: (res: any) => {
-          this.suggestions.set(res);
-          this.isLoading.set(false);
-        },
-        error: () => this.isLoading.set(false),
-      });
-
-      return () => sub.unsubscribe();
-    });
-
-  }
-
   ngOnInit() {
-  }
+    // Setup autocomplete filtering with debounce
+    this.filteredOptions = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      switchMap(value => {
+        const term = value || '';
 
-  onInputChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.inputTerm.set(input.value);
-  }
+        if (!term || term.length < 2) {
+          return of([]);
+        }
 
-  onSelectSuggestion(suggestion: string) {
-    this.inputTerm.set(suggestion);
-
-    this.search();
+        this.isLoading.set(true);
+        return this.solrService.getAutocompleteSuggestions(term).pipe(
+          switchMap(suggestions => {
+            this.isLoading.set(false);
+            return of(suggestions);
+          })
+        );
+      })
+    );
   }
 
   search() {
-    const query = this.inputTerm() || '';
-
+    const query = this.searchControl.value || '';
     this.searchService.search(query);
+  }
+
+  onOptionSelected(event: any) {
+    this.search();
   }
 }
