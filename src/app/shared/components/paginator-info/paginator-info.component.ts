0d302296745f4bdb24fcad8db2a26028daedb2ea
marianharbist <marianharbist@gmail.com>
2025-04-24 15:37:23 +0200

    created paginator info component

---
+++ src/app/shared/components/paginator-info/paginator-info.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-paginator-info',
+  imports: [
+    TranslatePipe,
+  ],
+  templateUrl: './paginator-info.component.html',
+  styleUrl: './paginator-info.component.scss',
+  standalone: true
+})
+export class PaginatorInfoComponent {
+  @Input() page: number = 1;
+  @Input() pageSize: number = 10;
+  @Input() totalCount: number = 0;
+
+  get start(): number {
+    return this.totalCount === 0 ? 0 : (this.page - 1) * this.pageSize + 1;
+  }
+
+  get end(): number {
+    return Math.min(this.page * this.pageSize, this.totalCount);
+  }
+
+}
