812e7ae70639b3c478ce609f40dbc79074c20617
marianharbist <marianharbist@gmail.com>
2025-05-02 14:41:25 +0200

    updated toggle button group component - added showLabel,label and size options; added icon and iconColor as opt params in ToggleOption + updated functionality and style

--- src/app/shared/components/toggle-button-group/toggle-button-group.component.ts
+++ src/app/shared/components/toggle-button-group/toggle-button-group.component.ts
@@ -1,21 +1,28 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { NgForOf } from '@angular/common';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 export interface ToggleOption<T> {
+  icon?: string;
+  iconColor?: string;
   label: string;
   value: T;
 }
 
 @Component({
   selector: 'app-toggle-button-group',
-  imports: [NgForOf],
+  imports: [NgForOf, NgIf, TranslatePipe, NgClass],
   templateUrl: './toggle-button-group.component.html',
   styleUrl: './toggle-button-group.component.scss'
 })
 export class ToggleButtonGroupComponent<T = any> {
+
   @Input() options: ToggleOption<T>[] = [];
   @Input() value!: T;
+  @Input() showLabel = false;
+  @Input() label = '';
   @Output() valueChange = new EventEmitter<T>();
+  @Input() size: 'sm' | 'md' | 'lg' = 'sm';
 
   select(option: ToggleOption<T>) {
     if (option.value !== this.value) this.valueChange.emit(option.value);
