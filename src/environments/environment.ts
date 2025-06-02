424302e2a29f43577c2dd10b43702312bd17a259
rzeh4n <martin.rehanek@gmail.com>
2025-06-02 14:12:10 +0200

    configuration: collect buildtime and runtime configuration and log in HeaderComponent ngOnInit()

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+  // used by EnvironmentService
+  useStaticRuntimeConfig: true, // DŮLEŽITÉ: pokud je true, konfigurace se načítá z env.json; Pro produkci vždy true, pro lokální vývoj (environment.local.ts) false
+
+  // overriden with env.json if useStaticRuntimeConfig is true
+  devMode: true, // pro produkci ziskej z promenne APP_DEV_MODE (přes env.json)
+  environmentName: 'deployed (branch dev)', // pro produkci ziskej z promenne APP_ENV_NAME (přes env.json)
+  environmentCode: 'd_d', // pro produkci ziskej z promenne APP_ENV_CODE (přes env.json)
+
+  krameriusBaseUrl: '', // pro produkci ziskej z promenne APP_KRAMERIUS_URL (přes env.json)
+  
+};
