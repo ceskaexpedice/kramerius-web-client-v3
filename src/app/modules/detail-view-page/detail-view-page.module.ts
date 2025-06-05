5853ffe82b0c09889d18a6914b4044f840a59263
rzeh4n <martin.rehanek@gmail.com>
2025-06-05 12:01:03 +0200

    switch form hard-coded configuration to configuration form Environment

--- src/app/modules/detail-view-page/detail-view-page.module.ts
+++ src/app/modules/detail-view-page/detail-view-page.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core';
-import {DetailViewPageComponent} from './detail-view-page.component';
-import {RouterModule, Routes} from '@angular/router';
-import {StoreModule} from '@ngrx/store';
-import {EffectsModule} from '@ngrx/effects';
-import {documentDetailReducer} from '../../shared/state/document-detail/document-detail.reducer';
-import {DocumentDetailEffects} from '../../shared/state/document-detail/document-detail.effects';
-import {AsyncPipe, JsonPipe, NgIf} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DetailViewPageComponent } from './detail-view-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { documentDetailReducer } from '../../shared/state/document-detail/document-detail.reducer';
+import { DocumentDetailEffects } from '../../shared/state/document-detail/document-detail.effects';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 
 const routes: Routes = [
   {
@@ -27,4 +27,4 @@ const routes: Routes = [
   ],
 })
 
-export class DetailViewPageModule {}
+export class DetailViewPageModule { }
