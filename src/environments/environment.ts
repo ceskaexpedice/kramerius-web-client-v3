3dd39fcfff40df325d372e14a48c4c13d07e0391
rzeh4n <martin.rehanek@gmail.com>
2025-06-05 11:24:20 +0200

    update default config for branch main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   useStaticRuntimeConfig: true, // DŮLEŽITÉ: pokud je true, konfigurace se načítá z env.json; Pro produkci vždy true, pro lokální vývoj (environment.local.ts) false
 
   // overriden with env.json if useStaticRuntimeConfig is true
-  devMode: true, // pro produkci ziskej z promenne APP_DEV_MODE (přes env.json)
-  environmentName: 'deployed (branch dev)', // pro produkci ziskej z promenne APP_ENV_NAME (přes env.json)
-  environmentCode: 'd_d', // pro produkci ziskej z promenne APP_ENV_CODE (přes env.json)
+  devMode: false, // pro produkci ziskej z promenne APP_DEV_MODE (přes env.json)
+  environmentName: 'deployed (branch main)', // pro produkci ziskej z promenne APP_ENV_NAME (přes env.json)
+  environmentCode: 'd_m', // pro produkci ziskej z promenne APP_ENV_CODE (přes env.json)
 
   krameriusBaseUrl: 'https://api.kramerius.mzk.cz/search/api/client/v7.0/', // pro produkci ziskej z promenne APP_KRAMERIUS_URL (přes env.json)
 
