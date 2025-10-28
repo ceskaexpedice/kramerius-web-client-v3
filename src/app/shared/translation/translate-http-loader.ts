a2aa71bc34ecd792358c15b861e41c4588662178
marianharbist <marianharbist@gmail.com>
2025-10-28 15:28:32 +0100

    temporary lang fix - just reordered lang files

--- src/app/shared/translation/translate-http-loader.ts
+++ src/app/shared/translation/translate-http-loader.ts
@@ -3,11 +3,11 @@ import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
 
 export function HttpLoaderFactory(handler: HttpBackend) {
   return new MultiTranslateHttpLoader(handler, [
-    'i18n/',
+    'i18n/languages/',
     'i18n/constants/',
     'i18n/codetables/',
     'i18n/shared/',
-    'i18n/languages/',
     'i18n/physical_locations/',
+    'i18n/',
   ]);
 }
