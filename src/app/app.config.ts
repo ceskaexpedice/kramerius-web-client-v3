c0f095f1e0b159ecd7dc5ebf12637558e95b5751
marianharbist <marianharbist@gmail.com>
2025-12-15 07:10:32 +0100

    Update translations and increment translation version
    - Updated i18n files (sk, cs, en, pl) with corrected translations for `subject_names_personal`, `subject_names_corporate`, and `subject_temporals` facets and filters.
    - Incremented translation version to `1.0.1` in `app.config.ts`.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '1.0.0',
+  translationVersion: '1.0.1',
   contactEmail: 'digitalni.knihovna@mzk.cz'
 };
 
