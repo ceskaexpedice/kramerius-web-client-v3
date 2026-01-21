2f02dda40a281c6139628f9aa051ab87bb711f85
marianharbist <marianharbist@gmail.com>
2026-01-21 11:46:43 +0100

    Bump `translationVersion` to `1.0.2` in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '1.0.1',
+  translationVersion: '1.0.2',
   contactEmail: 'digitalni.knihovna@mzk.cz'
 };
 
