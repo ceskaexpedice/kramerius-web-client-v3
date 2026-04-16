ffccd6c2199809887d21d461681ff5244672c4c5
marianharbist <marianharbist@gmail.com>
2026-04-16 07:33:30 +0200

    Update `translationVersion` to 1.0.9 in app config.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '1.0.8',
+  translationVersion: '1.0.9',
   contactEmail: 'digitalniknihovna@mzk.cz'
 };
 
