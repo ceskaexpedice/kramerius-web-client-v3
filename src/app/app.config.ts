f238ff19b541a01d7db4eba5bf236f9bf0469b61
marianharbist <marianharbist@gmail.com>
2026-06-09 10:52:45 +0200

    Enhance folder share dialog: update layout, add copy link functionality, and improve localization strings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '2.0.4',
+  translationVersion: '2.0.5',
   contactEmail: 'digitalniknihovna@mzk.cz'
 };
 
--- src/app/shared/dialogs/folder-share-dialog/folder-share-dialog.component.ts
+++ src/app/shared/dialogs/folder-share-dialog/folder-share-dialog.component.ts
@@ -3,24 +3,27 @@ import { TranslatePipe } from '@ngx-translate/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { copyTextToClipboard } from '../../misc/misc-functions';
 import { ToastService } from '../../services/toast.service';
+import { InputComponent } from '../../components/input/input.component';
 
 @Component({
   selector: 'app-folder-share-dialog',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, InputComponent],
   templateUrl: './folder-share-dialog.component.html',
-  styleUrls: ['./folder-share-dialog.component.scss', '../generic-dialog.scss'],
+  styleUrls: ['./folder-share-dialog.component.scss'],
 })
 export class FolderShareDialogComponent {
   shareUrl = '';
+  folderName = '';
 
   @Output() close = new EventEmitter<void>();
 
   private toastService = inject(ToastService);
   private dialogRef = inject(MatDialogRef<FolderShareDialogComponent>, { optional: true });
-  private data = inject<{ uuid: string }>(MAT_DIALOG_DATA);
+  private data = inject<{ uuid: string; name: string }>(MAT_DIALOG_DATA);
 
   constructor() {
     this.shareUrl = `${window.location.origin}/folders/${this.data.uuid}`;
+    this.folderName = this.data.name;
   }
 
   onClose() {
