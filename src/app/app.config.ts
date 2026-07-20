ddebe3a71e2952bf762413aae5ffa1ca14b253b8
marianharbist <marianharbist@gmail.com>
2026-07-20 10:37:39 +0200

    Bump `translationVersion` to `2.1.0` in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '2.0.9',
+  translationVersion: '2.1.0',
   contactEmail: 'info@ceskadigitalniknihovna.cz'
 };
 
