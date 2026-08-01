9796cc3425f696d2c601f3a918ecf18279e71eac
marianharbist <marianharbist@gmail.com>
2026-08-01 14:31:57 +0200

    Refactor settings dialog: comment out experimental and GDPR sections for clarity

--- src/app/shared/dialogs/settings-dialog/settings-dialog.component.ts
+++ src/app/shared/dialogs/settings-dialog/settings-dialog.component.ts
@@ -42,8 +42,8 @@ export class SettingsDialogComponent implements OnInit {
       { key: 'reading', label: 'settings-section-reading', icon: 'icon-volume-high' },
       // { key: 'preferences', label: 'settings-section-user-preferences', icon: 'icon-settings-4' },
       { key: 'accessibility', label: 'settings-section-accessibility', icon: 'icon-accesibility' },
-      { key: 'experimental', label: 'settings-section-experimental', icon: 'icon-code-2' },
-      { key: 'gdpr', label: 'settings-section-gdpr', icon: 'icon-shield-tick' },
+      // { key: 'experimental', label: 'settings-section-experimental', icon: 'icon-code-2' },
+      // { key: 'gdpr', label: 'settings-section-gdpr', icon: 'icon-shield-tick' },
     ]
   };
 
