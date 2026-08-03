77945777bc31525c4d5618406eac9fb039b71202
marianharbist <marianharbist@gmail.com>
2026-08-03 15:18:42 +0200

    Add new library entries to Czech, English, Polish, and Slovak translations #152

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '2.1.1',
+  translationVersion: '2.1.2',
   contactEmail: 'info@ceskadigitalniknihovna.cz'
 };
 
