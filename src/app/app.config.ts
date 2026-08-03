1af3fb53c76139daa043b8b5b62c7da9ceb78014
marianharbist <marianharbist@gmail.com>
2026-08-03 11:26:18 +0200

    Update translations and branding for Czech Digital Library #140

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const ENVIRONMENT = {
   defaultLanguage: 'cs',
   fallbackLanguage: 'en',
   dateRangeStartYear: 1162,
-  translationVersion: '2.1.0',
+  translationVersion: '2.1.1',
   contactEmail: 'info@ceskadigitalniknihovna.cz'
 };
 
