ede287cffcbfde8c7fe4ee3a9d35c959bba30d19
marianharbist <marianharbist@gmail.com>
2026-09-01 14:50:46 +0200

    Implement mobile navigation bar and immersive mode; adjust styles and logic to stack with global mini-player (#172)

--- src/app/modules/music/music-page.module.ts
+++ src/app/modules/music/music-page.module.ts
@@ -25,6 +25,16 @@ import {InlineLoaderComponent} from '../../shared/components/inline-loader/inlin
 import {MetadataSidebarComponent} from '../../shared/components/metadata-sidebar/metadata-sidebar.component';
 import {TabsComponent} from '../../shared/components/tabs/tabs.component';
 import {TabItemComponent} from '../../shared/components/tabs/tab-item.component';
+import {MobileNavBarComponent} from '../../shared/components/mobile-nav-bar/mobile-nav-bar.component';
+import {ViewerTapToggleDirective} from '../../shared/directives/viewer-tap-toggle.directive';
+import {SlideUpPanelComponent} from '../../shared/components/slide-up-panel/slide-up-panel.component';
+import {MetadataSection} from '../../shared/components/metadata-section/metadata-section';
+import {
+  ExportDocumentSectionComponent
+} from '../../shared/components/metadata-sidebar/export-document-section-component/export-document-section-component';
+import {
+  SearchResultsSidebarComponent
+} from '../../shared/components/metadata-sidebar/search-results-sidebar/search-results-sidebar.component';
 
 const routes: Routes = [
   {
@@ -61,6 +71,12 @@ const routes: Routes = [
         MetadataSidebarComponent,
         TabsComponent,
         TabItemComponent,
+        MobileNavBarComponent,
+        ViewerTapToggleDirective,
+        SlideUpPanelComponent,
+        MetadataSection,
+        ExportDocumentSectionComponent,
+        SearchResultsSidebarComponent,
     ],
   providers: [
   ]
